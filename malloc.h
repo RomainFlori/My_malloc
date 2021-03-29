a3791d7eeb29f4458a5ef36d0e25ff0d60cdfe43
Romain Flori-Cantrelle <56433622+RomainFlori@users.noreply.github.com>
2021-03-29 15:04:25 +0200

    Update malloc.h

--- malloc.h
+++ malloc.h
@@ -13,21 +13,6 @@
 #include <assert.h>
 #include <string.h>
 
-typedef struct s_block {
-    size_t size;
-    void* adress;
-    int free;
-    struct s_block *next;
-} s_block_t;
-
-s_block_t *_glob_struct;
-
-s_block_t *request_space(size_t size);
-void *find_free_block(size_t size);
-
-
-#define STRUCT_SIZE	sizeof(struct s_block)
-
 void *malloc(size_t size);
 void *calloc(size_t nmemb, size_t size);
 void *realloc(void *ptr, size_t size);
